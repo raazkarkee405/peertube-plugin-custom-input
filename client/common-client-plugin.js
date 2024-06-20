2603307c89dfd246fc4a7e84da3c335f9faef4e5
durgaraj726 <durgaraj726@gmail.com>
2024-06-20 12:33:57 +0545

    adds custom tag input

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,7 +1,18 @@
-function register ({ registerHook, peertubeHelpers }) {
-  console.log('Hello world')
+import Tagify from "@yaireo/tagify";
+function register({ registerHook, peertubeHelpers, registerClientRoute }) {
+  registerClientRoute({
+    route: "/custom-route",
+    onMount: async ({ rootEl }) => {
+      const input = document.createElement("input");
+      input.name = "basic";
+      input.placeholder = "Enter here";
+      rootEl.append(input);
+
+      const inputField = document.querySelector("input[name=basic]");
+      new Tagify(inputField);
+    },
+  });
 }
 
-export {
-  register
-}
+export { register };
+
